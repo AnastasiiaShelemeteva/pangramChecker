58e1221267c08752220d57214a849907930bb39b
AnastasiiaShelemeteva <66690836+AnastasiiaShelemeteva@users.noreply.github.com>
2020-06-14 00:27:54 +0200

    Add files via upload

---
+++ pangramChecker.js
@@ -0,0 +1,11 @@
+function pangramChecker(input) {
+  input = input.toLowerCase();
+  const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  const missingLetters = alphabet.filter(n => input.indexOf(n) === -1);
+
+  return missingLetters.length === 0
+    ? 'This is a pangram'
+    : `This is not a pangram. Missing letters are ${missingLetters}`;
+
+}
+pangramChecker('fdshfs');
