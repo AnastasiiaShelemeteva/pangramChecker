67ae223783ab020c4456ee24c1d6a19ac3a9423a
AnastasiiaShelemeteva <66690836+AnastasiiaShelemeteva@users.noreply.github.com>
2020-06-14 10:27:29 +0200

    Update pangramChecker.js

--- pangramChecker.js
+++ pangramChecker.js
@@ -1,9 +1,9 @@
 function pangramChecker(input) {
-  input = input.toLowerCase();
+  input = input.toLowerCase(); // here we bring our input to lower case, so it doesn't affect, if we use upper case in input
   const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-  const missingLetters = alphabet.filter(n => input.indexOf(n) === -1);
+  const missingLetters = alphabet.filter(n => input.indexOf(n) === -1); // here we compare alphabet with our input
 
-  return missingLetters.length === 0
+  return missingLetters.length === 0 // means if missingLetters is empty
     ? 'This is a pangram'
     : `This is not a pangram. Missing letters are ${missingLetters}`;
 
